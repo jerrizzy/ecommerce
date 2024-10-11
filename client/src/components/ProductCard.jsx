7a049b4b669c457f6a58b07673b379a9f2fe4324
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-10-11 16:51:17 -0400

    productCard Component success

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom"
 
-function ProductCard({ id, name, image }) {
+function ProductCard({ id, product }) {
   return (
     <Link to={`/products/${id}`}>
    
     <div className="product-card">
-      <img src={image} alt={name} />
-      <h3>{name}</h3>
-      <p>{price}</p>
+      <img src={product.image} alt={name} />
+      <h3>{product.name}</h3>
+      <p>{product.price}</p>
       <button className="add-to-cart">Add to Cart ��</button>
     </div>
-    
+
     </Link>
   )
 }
