9c4c6d2a25e625413655e1bf6090d6fbddd43687
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-10-15 20:04:34 -0400

    productPage component success

--- client/src/components/ProductPage.jsx
+++ client/src/components/ProductPage.jsx
@@ -1,32 +1,33 @@
 import { React, useEffect } from 'react';
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, useParams } from "react-router-dom"
 
 function ProductPage() {
-  const { id } = useParams(); // we get the id from the URL parameters using this hook, and we capture the dynamic part of the URL that identifies a specific restaurant
-  const [foundProduct, setFoundProduct] = (null)
+  // const { id } = useParams(); // we get the id from the URL parameters using this hook, and we capture the dynamic part of the URL that identifies a specific restaurant
+  const [foundProduct, setFoundProduct] = ([])
   const products = useLoaderData()
+  console.log(products.name)
 
-  useEffect(() => {
-    const product = products.find(product => product.id === id);
-    if (product) {
-      setFoundProduct(product);
-    }
+  // useEffect(() => {
+  //   const product = products.find(product => product.id === id);
+  //   if (product) {
+  //     setFoundProduct(product);
+  //   }
 
-  }, [id, products])
+  // }, [id, products])
 
-  if (!foundProduct) {
+  if (!products) {
     return <div>Loading...</div>;  // Handle if item is not found or data is still loading
   }
 
   return (
     <div>
       <h1>Item details</h1>
-      <img src={foundProduct.image} />
-      <h2>{foundProduct.name}</h2>
-      <p>Brand: {foundProduct.brand}</p>
-      <p>Description: {foundProduct.description}</p>
-      <p>Available quantity: {foundProduct.quantity}</p>
-      <p>Rating: {foundProduct.rating}/5</p>
+      <img src={products.image} />
+      <h2>{products.name}</h2>
+      <p>Brand: {products.brand}</p>
+      <p>Description: {products.description}</p>
+      <p>Available quantity: {products.quantity}</p>
+      <p>Rating: {products.rating}/5</p>
     </div>
   )
 }
